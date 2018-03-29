04926ce516fa26386c2ab72b9167ed4c49b65777
WaelAlnadeem <wael.alnadeem88@gmail.com>
2018-03-29 20:35:16 +0200

    rewrite the code

--- Week 1/app.js
+++ Week 1/app.js
@@ -2,30 +2,63 @@
 
 const APIUrl = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
-document.getElementById("root");
-const header = document.createElement('div');
-root.appendChild(header);
-const head = document.createElement('h2');
-header.appendChild(head);
-head.innerHTML = "HYF Repositories"
-const select = document.createElement('select');
-header.appendChild(select);
-const main = document.createElement('div');
-root.appendChild(main);
-const details = document.createElement('div');
-main.appendChild(details);
-const contributer = document.createElement('div');
-main.appendChild(contributer);
-const contributions = document.createElement('h2');
+function main() {
+    document.getElementById("root");
 
+    const header = createAndAppend('div', root, { class: 'header' });
+    createAndAppend('h2', header, { class: 'head', html: 'HYF Repositories' });
+    const select = createAndAppend('select', header, { class: 'select' });
+    const main = createAndAppend('div', root, { class: 'main' });
+    const details = createAndAppend('div', main, { class: 'details' });
+    const contributor = createAndAppend('div', main, { class: 'contributor' });
 
-main.className = "main";
-select.className = "select";
-details.className = "details";
-contributer.className = "contributer";
-header.className = "header";
-head.className = "head";
-contributions.className = 'contributionsHead';
+    const newData = {};
+    fetchJSON(APIUrl, function (error, data) {
+        if (error) {
+            root.innerHTML = error.message;
+            return;
+        }
+        data.forEach(function (element) {
+            newData[element.name] = {
+                name: element.name,
+                description: element.description,
+                forks: element.forks,
+                updatedAt: element.updated_at,
+                contribute: element.url,
+                gitHubAdress: element.html_url,
+            };
+        });
+        const keysOfData = Object.keys(newData);
+        keysOfData.forEach(element => {
+            const option = document.createElement('option');
+            option.innerHTML = element;
+            option.value = element;
+            select.appendChild(option);
+        });
+        result(newData[select.selectedOptions[0].innerText]);
+    });
+
+    select.addEventListener('change', event => {
+        const repoName = event.target.value;
+        details.innerHTML = '';
+        contributor.innerHTML = '';
+        result(newData[repoName]);
+    });
+}
+
+function createAndAppend(name, parent, options = {}) {
+    const elem = document.createElement(name);
+    parent.appendChild(elem);
+    Object.keys(options).forEach(key => {
+        const value = options[key];
+        if (key === 'html') {
+            elem.innerHTML = value;
+        } else {
+            elem.setAttribute(key, value);
+        }
+    });
+    return elem;
+}
 
 function fetchJSON(url, cb) {
     const xhr = new XMLHttpRequest();
@@ -44,41 +77,14 @@ function fetchJSON(url, cb) {
     xhr.send();
 }
 
-const newData = {};
-fetchJSON(APIUrl, function (error, data) {
-    data.forEach(function (element) {
-        newData[element.name] = {
-            name: element.name,
-            description: element.description,
-            forks: element.forks,
-            updated_at: element.updated_at,
-            contribute: element.url,
-            gitHubAdress: element.html_url,
-        };
-    });
-
-    const keysOfData = Object.keys(newData);
-    keysOfData.forEach(element => {
-        const option = document.createElement('option');
-        option.innerHTML = element;
-        option.value = element;
-        select.appendChild(option);
-    });
-    result(newData[select.selectedOptions[0].innerText]);
-});
-select.addEventListener('change', event => {
-    console.log(event.target.value);
-    const repoName = event.target.value;
-    details.innerHTML = '';
-    contributer.innerHTML = '';
-    result(newData[repoName]);
-});
-
 function result(repoData) {
-    contributer.appendChild(contributions);
+    const details = document.querySelector('.details');
+    const contributor = document.querySelector('.contributor');
+    const contributions = document.createElement('h2');
+    contributor.appendChild(contributions);
     contributions.innerHTML = "contributions";
     const detailsText = document.createElement('p');
-    detailsText.innerHTML = 'name : <a href = "' + repoData.gitHubAdress + '">  ' + repoData.name + ' </a><br>' + 'description : ' + repoData.description + '<br>' + 'forks : ' + repoData.forks + '<br>' + 'updated : ' + repoData.updated_at;
+    detailsText.innerHTML = 'name : <a href = "' + repoData.gitHubAdress + '">  ' + repoData.name + ' </a><br>' + 'description : ' + repoData.description + '<br>' + 'forks : ' + repoData.forks + '<br>' + 'updated : ' + repoData.updatedAt;
     detailsText.className = "detailsText";
     details.appendChild(detailsText);
     fetchJSON(repoData.contribute, function (error, data) {
@@ -87,19 +93,20 @@ function result(repoData) {
                 const img = document.createElement('img');
                 img.setAttribute('src', data[i].avatar_url);
                 img.setAttribute("class", "Img");
-                contributer.appendChild(img);
+                contributor.appendChild(img);
                 const contribution = document.createElement('p');
                 contribution.className = "contribution";
                 contribution.innerHTML = data[i].contributions;
                 contribution.className = "contribution";
-                contributer.appendChild(contribution);
+                contributor.appendChild(contribution);
                 const login = document.createElement('h3');
                 login.className = "login";
                 login.innerHTML = data[i].login;
-                contributer.appendChild(login);
+                contributor.appendChild(login);
 
             }
         });
     });
 
 }
+window.onload = main;
