e3f9ce6e7d3830080e71a40867c1e111b5e898f3
WaelAlnadeem <wael.alnadeem88@gmail.com>
2018-03-29 20:44:33 +0200

    rewrite the code

--- Week 2/app.js
+++ Week 2/app.js
@@ -1,28 +1,67 @@
 'use strict';
 const APIUrl = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
-document.getElementById("root");
-const header = document.createElement('div');
-root.appendChild(header);
-const head = document.createElement('h2');
-header.appendChild(head);
-head.innerHTML = "HYF Repositories";
-const select = document.createElement('select');
-header.appendChild(select);
-const main = document.createElement('div');
-root.appendChild(main);
-const details = document.createElement('div');
-main.appendChild(details);
-const contributor = document.createElement('div');
-main.appendChild(contributor);
-const contributions = document.createElement('h2');
-main.className = "main";
-select.className = "select";
-details.className = "details";
-contributor.className = "contributor";
-header.className = "header";
-head.className = "head";
-contributions.className = 'contributionsHead';
+
+function main() {
+    document.getElementById("root");
+
+    const header = createAndAppend('div', root, { class: 'header' });
+    createAndAppend('h2', header, { class: 'head', html: 'HYF Repositories' });
+    const select = createAndAppend('select', header, { class: 'select' });
+    const main = createAndAppend('div', root, { class: 'main' });
+    const details = createAndAppend('div', main, { class: 'details' });
+    const contributor = createAndAppend('div', main, { class: 'contributor' });
+
+    const newData = {};
+    fetchJSON(APIUrl)
+        .then(data => {
+            data.forEach(element => {
+                newData[element.name] = {
+                    name: element.name,
+                    description: element.description,
+                    forks: element.forks,
+                    updatedAt: element.updated_at,
+                    contribute: element.url,
+                    gitHubAdress: element.html_url,
+                };
+            });
+            const keysOfData = Object.keys(newData);
+            keysOfData.forEach(element => {
+                const option = document.createElement('option');
+                option.className = "option";
+                option.innerHTML = element;
+                option.value = element;
+                select.appendChild(option);
+            });
+            return (newData[select.selectedOptions[0].innerText]);
+        })
+        .then(data => { result(data); })
+        .catch(err => {
+            root.innerHTML = err.message;
+            alert("err.message");
+        });
+
+    select.addEventListener('change', event => {
+        const repoName = event.target.value;
+        details.innerHTML = '';
+        contributor.innerHTML = '';
+        result(newData[repoName]);
+    });
+}
+
+function createAndAppend(name, parent, options = {}) {
+    const elem = document.createElement(name);
+    parent.appendChild(elem);
+    Object.keys(options).forEach(key => {
+        const value = options[key];
+        if (key === 'html') {
+            elem.innerHTML = value;
+        } else {
+            elem.setAttribute(key, value);
+        }
+    });
+    return elem;
+}
 
 function fetchJSON(url) {
     return new Promise((resolve, reject) => {
@@ -41,42 +80,11 @@ function fetchJSON(url) {
         xhr.send();
     });
 }
-const newData = {};
-fetchJSON(APIUrl)
-    .then(data => {
-        data.forEach(element => {
-            newData[element.name] = {
-                name: element.name,
-                description: element.description,
-                forks: element.forks,
-                updatedAt: element.updated_at,
-                contribute: element.url,
-                gitHubAdress: element.html_url,
-            };
-        });
-        const keysOfData = Object.keys(newData);
-        keysOfData.forEach(element => {
-            const option = document.createElement('option');
-            option.className = "option";
-            option.innerHTML = element;
-            option.value = element;
-            select.appendChild(option);
-        });
-        return (newData[select.selectedOptions[0].innerText]);
-    })
-    .then(data => { result(data); })
-    .catch(err => {
-        root.innerHTML = err.message;
-        alert("err.message");
-    });
 
-select.addEventListener('change', event => {
-    const repoName = event.target.value;
-    details.innerHTML = '';
-    contributor.innerHTML = '';
-    result(newData[repoName]);
-});
 function result(repoData) {
+    const details = document.querySelector('.details');
+    const contributor = document.querySelector('.contributor');
+    const contributions = document.createElement('h2');
     contributor.appendChild(contributions);
     contributions.innerHTML = "contributions";
     const detailsText = document.createElement('p');
@@ -109,3 +117,4 @@ function result(repoData) {
             alert("err.message");
         });
 }
+window.onload = main;
